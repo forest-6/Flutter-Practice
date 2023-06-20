6a533f771f5195407094f261ebd83185fef2ac5c
forest-6 <seungmin3238@gmail.com>
2023-06-20 19:13:20 +0900

    AppBar, BottomNavBar 구현

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,29 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title:  Text("DOGS"),
+          leading:  Icon(Icons.menu),
+          centerTitle: false,
+          backgroundColor: Colors.brown[800],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.camera_alt, color: Colors.brown[600],),
+                Icon(Icons.home, color: Colors.brown[600],),
+                Icon(Icons.message, color: Colors.brown[600],),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
